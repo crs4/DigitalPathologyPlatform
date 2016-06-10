208cb9733701c6f74ace19cac1e961fbc2446c6b
Luca Lianas <luca.lianas@crs4.it>
2016-06-10 16:48:05 +0200

    adding logout button behaviour

---
+++ promort/src/js/layout/layout.modules.js
@@ -0,0 +1,11 @@
+(function() {
+    'use strict';
+    
+    angular
+        .module('promort.layout', [
+            'promort.layout.controllers'
+        ]);
+    
+    angular
+        .module('promort.layout.controllers', []);
+})();
