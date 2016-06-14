c9b06b64465b1dbdaa8a6b72e127a867bde144ac
Luca Lianas <luca.lianas@crs4.it>
2016-06-14 17:20:15 +0200

    Case and Slide models

--- promort/promort/settings.py
+++ promort/promort/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'rest_framework',
     'authentication',
+    'slides_manager',
 )
 
 MIDDLEWARE_CLASSES = (
---
+++ promort/slides_manager/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Case(models.Model):
+    id = models.CharField(max_length=10, primary_key=True)
+    import_date = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return 'Case %s' % self.id
+
+    def _get_slides_list(self):
+        return self.slide_set.all()
+    slides = property(_get_slides_list)
+
+
+class Slide(models.Model):
+    id = models.CharField(max_length=25, primary_key=True)
+    case = models.ForeignKey(Case, on_delete=models.PROTECT,
+                             blank=False)
+    import_date = models.DateTimeField(auto_now_add=True)
+    omero_id = models.IntegerField(blank=True, null=True)
+    image_type = models.CharField(max_length=5, blank=False)
+
+    def __unicode__(self):
+        return 'Slide %s [img type: %s --- OMERO id: %r]' % (self.id, self.image_type,
+                                                             self.omero_id)
