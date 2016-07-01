54bc9b5372bb01e7312ed131dc14d93b6471677b
Luca Lianas <luca.lianas@crs4.it>
2016-07-01 17:02:30 +0200

    new route

--- promort/src/js/promort.routes.js
+++ promort/src/js/promort.routes.js
@@ -8,10 +8,17 @@
     config.$inject = ['$routeProvider'];
 
     function config($routeProvider) {
-        $routeProvider.when('/login', {
-            controller: 'LoginController',
-            controllerAs: 'vm',
-            templateUrl: '/static/templates/authentication/login.html'
-        }).otherwise('/');
+        $routeProvider
+            .when('/login', {
+                controller: 'LoginController',
+                controllerAs: 'vm',
+                templateUrl: '/static/templates/authentication/login.html'
+            })
+            .when('/worklist', {
+                controller: 'WorkListController',
+                controllerAs: 'wlc',
+                templateUrl: '/static/templates/worklist/pending_reviews.html'
+            })
+            .otherwise('/');
     }
 })();
