be1ce5918514e7e3a153d1a66efbf4884e24f8ba
Luca Lianas <luca.lianas@crs4.it>
2016-07-19 14:39:03 +0200

    email configuration

--- promort/promort/settings.py
+++ promort/promort/settings.py
@@ -167,6 +167,13 @@ REST_FRAMEWORK = {
 SESSION_COOKIE_NAME = 'promort_sessionid'
 
 
+# email settings
+EMAIL_USE_TLS = True
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = None
+
 # CUSTOM SETTINGS
 DEFAULT_GROUPS = {
     'reviewer_1': {
