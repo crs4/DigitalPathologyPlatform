fbb5866d26a4965595cc04ab82675a68cfcc8a6f
Luca Lianas <luca.lianas@crs4.it>
2016-09-26 13:40:46 +0200

    ROIs forms simple directives

--- promort/src/js/rois_manager/rois_manager.directives.js
+++ promort/src/js/rois_manager/rois_manager.directives.js
@@ -2,5 +2,35 @@
     'use strict';
 
     angular
-        .module('promort.rois_manager.directives');
+        .module('promort.rois_manager.directives')
+        .directive('newSliceCreation', newSliceCreation)
+        .directive('newCoreCreation', newCoreCreation)
+        .directive('newFocusRegionCreation', newFocusRegionCreation);
+
+    function newSliceCreation() {
+        var directive = {
+            replace: true,
+            restrict: 'E',
+            templateUrl: '/static/templates/rois_manager/slice.html'
+        };
+        return directive;
+    }
+
+    function newCoreCreation() {
+        var directive = {
+            replace: true,
+            restrict: 'E',
+            templateUrl: '/static/templates/rois_manager/core.html'
+        };
+        return directive;
+    }
+
+    function newFocusRegionCreation() {
+        var directive = {
+            replace: true,
+            restrict: 'E',
+            templateUrl: '/static/templates/rois_manager/region.html'
+        };
+        return directive;
+    }
 })();
