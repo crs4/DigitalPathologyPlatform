9a53c133b2c4fa538bf807879c2c48a78c97935a
Luca Lianas <luca.lianas@crs4.it>
2017-02-23 15:24:46 +0100

    ROIs tree panel

--- promort/src/js/clinical_annotations_manager/clinical_annotations_manager.controllers.js
+++ promort/src/js/clinical_annotations_manager/clinical_annotations_manager.controllers.js
@@ -5,16 +5,22 @@
         .module('promort.clinical_annotations_manager.controllers')
         .controller('ClinicalAnnotationsManagerController', ClinicalAnnotationsManagerController);
 
-    ClinicalAnnotationsManagerController.$inject = ['$scope', '$routeParams', '$compile', '$location',
+    ClinicalAnnotationsManagerController.$inject = ['$scope', '$rootScope', '$routeParams', '$compile', '$location',
         'ngDialog', 'Authentication', 'AnnotationsViewerService', 'ClinicalAnnotationStepService'];
 
-    function ClinicalAnnotationsManagerController($scope, $routeParams, $compile, $location, ngDialog, Authentication,
-                                                  AnnotationsViewerService, ClinicalannotationStepService) {
+    function ClinicalAnnotationsManagerController($scope, $rootScope, $routeParams, $compile, $location, ngDialog,
+                                                  Authentication, AnnotationsViewerService,
+                                                  ClinicalannotationStepService) {
         var vm = this;
         vm.slide_id = undefined;
         vm.case_id = undefined;
         vm.rois_annotation_step_id = undefined;
         vm.clinical_annotation_step_id = undefined;
+
+        vm.slices_map = undefined;
+        vm.cores_map = undefined;
+        vm.focus_regions_map = undefined;
+
 
         vm.ui_active_mode = {
             'annotate_slice': false,
@@ -26,6 +32,18 @@
         };
         vm.roisTreeLocked = false;
 
+        vm._registreSlice = _registerSlice;
+        vm._registerCore = _registerCore;
+        vm._registerFocusRegion = _registerFocusRegion;
+        vm._getSliceLabel = _getSliceLabel;
+        vm._getCoreLabel = _getCoreLabel;
+        vm._getFocusRegionLabel = _getFocusRegionLabel;
+        vm._createListItem = _createListItem;
+        vm._createNewSubtree = _createNewSubtree;
+        vm.showROIPanel = showROIPanel;
+        vm.selectROI = selectROI;
+        vm.deselectROI = deselectROI;
+
         activate();
 
         function activate() {
@@ -33,6 +51,14 @@
             vm.case_id = $routeParams.case;
             vm.rois_annotation_step_id = $routeParams.annotation_step;
             vm.clinical_annotation_step_id = $routeParams.clinical_annotation_step;
+
+            vm.slices_map = [];
+            vm.cores_map = [];
+            vm.focus_regions_map = [];
+
+            $rootScope.slices = [];
+            $rootScope.cores = [];
+            $rootScope.focus_regions = [];
 
             ClinicalannotationStepService.getDetails(vm.case_id, Authentication.getCurrentUser(),
                 vm.rois_annotation_step_id, vm.slide_id)
@@ -42,11 +68,139 @@
                 if (response.data.completed === true || response.data.can_be_started === false) {
                     $location.url('worklist/' + vm.case_id);
                 }
+
+                $scope.$on('slice.new',
+                    function(event, slice_info) {
+                        vm._registreSlice(slice_info);
+                        var $tree = $("#rois_tree");
+                        var $new_slice_item = $(vm._createListItem(slice_info.label, true));
+                        var $anchor = $new_slice_item.find('a');
+                        $anchor.attr('ng-click', 'cmc.showROIPanel("slice", ' + slice_info.id + ')')
+                            .attr('ng-mouseenter', 'cmc.selectROI("slice", ' + slice_info.id + ')')
+                            .attr('ng-mouseleave', 'cmc.deselectROI("slice", ' + slice_info.id + ')');
+                        $compile($anchor)($scope);
+                        var new_slice_subtree = vm._createNewSubtree(slice_info.label);
+                        $new_slice_item.append(new_slice_subtree);
+                        $tree.append($new_slice_item);
+                    }
+                );
+
+                $scope.$on('core.new',
+                    function(event, core_info) {
+                        vm._registerCore(core_info);
+                        var $tree = $("#" + vm._getSliceLabel(core_info.slice) + "_tree");
+                        var $new_core_item = $(vm._createListItem(core_info.label, true));
+                        var $anchor = $new_core_item.find('a');
+                        $anchor.attr('ng-click', 'cmc.showROIPanel("core", ' + core_info.id + ')')
+                            .attr('ng-mouseenter', 'cmc.selectROI("core", ' + core_info.id + ')')
+                            .attr('ng-mouseleave', 'cmc.deselectROI("core", ' + core_info.id + ')');
+                        $compile($anchor)($scope);
+                        var new_core_subtree = vm._createNewSubtree(core_info.label);
+                        $new_core_item.append(new_core_subtree);
+                        $tree.append($new_core_item);
+                    }
+                );
+
+                $scope.$on('focus_region.new',
+                    function(event, focus_region_info) {
+                        vm._registerFocusRegion(focus_region_info);
+                        var $tree = $("#" + vm._getCoreLabel(focus_region_info.core) + "_tree");
+                        var $new_focus_region_item = $(vm._createListItem(focus_region_info.label, false));
+                        var $anchor = $new_focus_region_item.find('a');
+                        $anchor.attr('ng-click', 'cmc.showROIPanel("focus_region", ' + focus_region_info.id +', "' +
+                            vm._getCoreLabel(focus_region_info.core) + '")')
+                            .attr('ng-mouseenter', 'cmc.selectROI("focus_region", ' + focus_region_info.id + ')')
+                            .attr('ng-mouseleave', 'cmc.deselectROI("focus_region", ' + focus_region_info.id + ')');
+                        $compile($anchor)($scope);
+                        $tree.append($new_focus_region_item);
+                    }
+                );
             }
 
             function getClinicalAnnotationStepErrorFn(response) {
-                console.error('Cannon load slide info');
+                console.error('Cannot load slide info');
                 console.error(response);
+            }
+        }
+
+        function _registerSlice(slice_info) {
+            $rootScope.slices.push(slice_info);
+            vm.slices_map[slice_info.id] = slice_info.label;
+        }
+
+        function _getSliceLabel(slice_id) {
+            return vm.slices_map[slice_id];
+        }
+
+        function _registerCore(core_info) {
+            $rootScope.cores.push(core_info);
+            vm.cores_map[core_info.id] = core_info.label;
+        }
+
+        function _getCoreLabel(core_id) {
+            return vm.cores_map[core_id];
+        }
+
+        function _registerFocusRegion(focus_region_info) {
+            $rootScope.focus_regions.push(focus_region_info);
+            vm.focus_regions_map[focus_region_info.id] = focus_region_info.label;
+        }
+
+        function _getFocusRegionLabel(focus_region_id) {
+            return vm.focus_regions_map[focus_region_id];
+        }
+
+        function _createListItem(label, set_neg_margin_cls) {
+            var html = '<li id="';
+            html += label;
+            html += '_list" class="list-group-item prm-tree-item';
+            if (set_neg_margin_cls) {
+                html += ' prm-tree-item-neg-margin';
+            }
+            html += '"><a class="prm-tree-el" href="#"><i class="icon-black_question"></i> ';
+            html += label;
+            html += '</a></li>';
+            return html;
+        }
+
+        function _createNewSubtree(roi_label) {
+            var html = '<ul id="' + roi_label +'_tree" class="list-group"></ul>';
+            return html;
+        }
+
+        function showROIPanel(roi_type, roi_id) {
+            console.log('ROI: ' + roi_type + ' ' + roi_id);
+        }
+
+        function selectROI(roi_type, roi_id) {
+            if (!vm.roisTreeLocked) {
+                switch (roi_type) {
+                    case 'slice':
+                        AnnotationsViewerService.selectShape(vm._getSliceLabel(roi_id));
+                        break;
+                    case 'core':
+                        AnnotationsViewerService.selectShape(vm._getCoreLabel(roi_id));
+                        break;
+                    case 'focus_region':
+                        AnnotationsViewerService.selectShape(vm._getFocusRegionLabel(roi_id));
+                        break;
+                }
+            }
+        }
+
+        function deselectROI(roi_type, roi_id) {
+            if (!vm.roisTreeLocked) {
+                switch (roi_type) {
+                    case 'slice':
+                        AnnotationsViewerService.deselectShape(vm._getSliceLabel(roi_id));
+                        break;
+                    case 'core':
+                        AnnotationsViewerService.deselectShape(vm._getCoreLabel(roi_id));
+                        break;
+                    case 'focus_region':
+                        AnnotationsViewerService.deselectShape(vm._getFocusRegionLabel(roi_id));
+                        break;
+                }
             }
         }
     }
