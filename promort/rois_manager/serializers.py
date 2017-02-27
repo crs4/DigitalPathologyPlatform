3221942bf4ccab9c45a34f05965c95f40f6b098b
Luca Lianas <luca.lianas@crs4.it>
2017-02-27 10:37:59 +0100

    bug fix

--- promort/rois_manager/serializers.py
+++ promort/rois_manager/serializers.py
@@ -31,11 +31,10 @@ class SliceSerializer(serializers.ModelSerializer):
 
     def get_positive_cores_count(self, obj):
         positive_cores_counter = 0
-        cores = CoreSerializer(data=obj.cores.all(), many=True)
-        if cores.is_valid():
-            for core in cores.validated_data:
-                if core.positive:
-                    positive_cores_counter += 1
+        cores = CoreSerializer(obj.cores.all(), many=True)
+        for core in cores.data:
+            if core['positive']:
+                positive_cores_counter += 1
         return positive_cores_counter
 
     def validate_roi_json(self, value):
