b90004b5a49ffbf3c1a780b5d1321f11432fdb1b
Luca Lianas <luca.lianas@crs4.it>
2017-05-26 13:25:17 +0200

    new service used to hold information on the currently opened slide

--- promort/src/js/slides_manager/slides_manager.services.js
+++ promort/src/js/slides_manager/slides_manager.services.js
@@ -3,8 +3,51 @@
     
     angular
         .module('promort.slides_manager.services')
+        .factory('CurrentSlideDetailsService', CurrentSlideDetailsService)
         .factory('SlideService', SlideService)
         .factory('QualityControlService', QualityControlService);
+
+    CurrentSlideDetailsService.$inject = ['$http'];
+
+    function CurrentSlideDetailsService($http) {
+        var slideID = undefined;
+        var caseID = undefined;
+
+        var CurrentSlideDetailsService = {
+            getSlideByAnnotationStep: getSlideByAnnotationStep,
+            registerCurrentSlide: registerCurrentSlide,
+            getSlideId: getSlideId,
+            getCaseId: getCaseId
+        };
+
+        return CurrentSlideDetailsService;
+
+        function getSlideByAnnotationStep(annotation_step_label, annotation_type) {
+            slideID = undefined;
+            caseID = undefined;
+
+            switch (annotation_type) {
+                case 'ROIS_ANNOTATION':
+                    return $http.get('api/rois_annotations/steps/' + annotation_step_label + '/');
+                case 'CLINICAL_ANNOTATION':
+                    return $http.get('api/clinical_annotations/steps/' + annotation_step_label + '/');
+            }
+        }
+
+        function registerCurrentSlide(slide_id, case_id) {
+            console.log('REGISTERING SLIDE ' + slide_id + ' FOR CASE ' + case_id);
+            slideID = slide_id;
+            caseID = case_id;
+        }
+
+        function getSlideId() {
+            return slideID;
+        }
+
+        function getCaseId() {
+            return caseID;
+        }
+    }
 
     SlideService.$inject = ['$http'];
 
