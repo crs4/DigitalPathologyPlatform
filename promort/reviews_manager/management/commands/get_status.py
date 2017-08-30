2d357fec7e3a193c1f4ec9abad5c5c6bb75ad5cd
Luca Lianas <luca.lianas@crs4.it>
2017-08-30 16:34:18 +0200

    using ReviewsComparison object to check reviews status

--- promort/reviews_manager/management/commands/get_status.py
+++ promort/reviews_manager/management/commands/get_status.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from reviews_manager.models import ROIsAnnotation
+from reviews_manager.models import ReviewsComparison
 
 from collections import Counter
 import logging
@@ -15,24 +15,24 @@ class Command(BaseCommand):
         stats_counter = Counter()
         reviewers_counter = Counter()
 
-        rois_annotations = ROIsAnnotation.objects.all()
+        reviews_comparisons = ReviewsComparison.objects.all()
 
-        stats_counter['rois_annotations'] = len(rois_annotations)
+        stats_counter['review_comparisons'] = len(reviews_comparisons)
 
-        for annotation in rois_annotations:
-            if annotation.clinical_annotations_completed():
-                stats_counter['completed_annotations'] += 1
+        for rcomp in reviews_comparisons:
+            if rcomp.is_completed():
+                stats_counter['completed_review_comparisons'] += 1
             else:
-                stats_counter['not_completed_annotations'] += 1
-                for cl_annotation in annotation.clinical_annotations.all():
+                stats_counter['not_completed_review_comparisons'] += 1
+                for cl_annotation in (rcomp.review_1, rcomp.review_2):
                     if not cl_annotation.is_completed():
-                        reviewers_counter[cl_annotation.reviewer.username] += 1
+                        reviewers_counter[cl_annotation.clinical_annotation.reviewer.username] += 1
         logger.info('Data collected')
         logger.info('############################')
-        logger.info('ROIs annotations count: %d', stats_counter['rois_annotations'])
-        logger.info('Completed ROIs annotation(s): %d', stats_counter['completed_annotations'])
-        logger.info('Unfinished ROIs annotation(s): %d', stats_counter['not_completed_annotations'])
-        if stats_counter['not_completed_annotations'] > 0:
+        logger.info('ROIs annotations count: %d', stats_counter['review_comparisons'])
+        logger.info('Completed ROIs annotation(s): %d', stats_counter['completed_review_comparisons'])
+        logger.info('Unfinished ROIs annotation(s): %d', stats_counter['not_completed_review_comparisons'])
+        if stats_counter['not_completed_review_comparisons'] > 0:
             logger.info('--- Stats for each reviewer ---')
             for reviewer, counter in reviewers_counter.iteritems():
                 logger.info('Reviewer "%s" still needs to complete %d clinical review(s)', reviewer, counter)
