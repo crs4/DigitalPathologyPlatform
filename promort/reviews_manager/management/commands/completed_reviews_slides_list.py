fa5a8a555bd01a6a7fda1050da1677ed6072d0cd
Luca Lianas <luca.lianas@crs4.it>
2017-08-31 11:49:52 +0200

    added control to export only slides with an active link to an OMERO image

--- promort/reviews_manager/management/commands/completed_reviews_slides_list.py
+++ promort/reviews_manager/management/commands/completed_reviews_slides_list.py
@@ -1,6 +1,10 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
 from reviews_manager.models import ReviewsComparison
+
+import logging
+
+logger = logging.getLogger('promort_commands')
 
 
 class Command(BaseCommand):
@@ -9,18 +13,33 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('--output_file', dest='out_file', type=str, required=True,
                             help='the output file for the slides list')
+        parser.add_argument('--linked_only', action='store_true',
+                            help='write in the output file only slides linked to images on the OMERO server')
 
-    def _get_slides_list(self, reviews_comparison):
+    def _get_slide(self, reviews_comparison, ome_link_active):
         # review_1 and review_2 point to the same rois_review_step so it is safe to use review_1's
         # slide field to extract the ID
-        return reviews_comparison.review_1.slide.id
+        slide = reviews_comparison.review_1.slide
+        if ome_link_active:
+            if slide.omero_id is not None:
+                return slide.id
+        else:
+            return slide.id
 
     def handle(self, *args, **opts):
+        logger.info('=== Exporting list of slides with a complete review workflow ===')
         completed_slides_list = []
         review_comparions = ReviewsComparison.objects.all()
+        logger.info('Loaded %d ReviewsComparison objects', len(review_comparions))
         for rc in review_comparions:
             if rc.is_completed():
-                completed_slides_list.extend(self._get_slides_list(rc))
-        with open(opts['out_file'], 'w') as ofile:
-            for slide in completed_slides_list:
-                ofile.write('%s\n' % slide)
+                s = self._get_slide(rc, opts['linked_only'])
+                if s is not None:
+                    completed_slides_list.extend(s)
+        logger.info('%d slides related to a completed review workflow', len(completed_slides_list))
+        if len(completed_slides_list) > 0:
+            logger.info('Writing output file')
+            with open(opts['out_file'], 'w') as ofile:
+                for slide in completed_slides_list:
+                    ofile.write('%s\n' % slide)
+        logger.info('=== Export complete ===')
