28293ad843fd8f6633b4fbcc3393be78459faabf
Luca Lianas <luca.lianas@crs4.it>
2017-08-31 09:15:46 +0200

    using ReviewsComparison objects to check for which slides we have the completed review cycle

--- promort/reviews_manager/management/commands/completed_reviews_slides_list.py
+++ promort/reviews_manager/management/commands/completed_reviews_slides_list.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 
-from reviews_manager.models import ROIsAnnotation
+from reviews_manager.models import ReviewsComparison
 
 
 class Command(BaseCommand):
@@ -10,15 +10,17 @@ class Command(BaseCommand):
         parser.add_argument('--output_file', dest='out_file', type=str, required=True,
                             help='the output file for the slides list')
 
-    def _get_slides_list(self, annotation):
-        return [step.slide.id for step in annotation.steps.all()]
+    def _get_slides_list(self, reviews_comparison):
+        # review_1 and review_2 point to the same rois_review_step so it is safe to use review_1's
+        # slide field to extract the ID
+        return reviews_comparison.review_1.slide.id
 
     def handle(self, *args, **opts):
         completed_slides_list = []
-        rois_annotations = ROIsAnnotation.objects.all()
-        for annotation in rois_annotations:
-            if annotation.clinical_annotations_completed():
-                completed_slides_list.extend(self._get_slides_list(annotation))
+        review_comparions = ReviewsComparison.objects.all()
+        for rc in review_comparions:
+            if rc.is_completed():
+                completed_slides_list.extend(self._get_slides_list(rc))
         with open(opts['out_file'], 'w') as ofile:
             for slide in completed_slides_list:
                 ofile.write('%s\n' % slide)
