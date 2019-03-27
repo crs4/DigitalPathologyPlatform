9c3f22ccc3ca985d11429f392d3037665454bb7c
Luca Lianas <luca.lianas@crs4.it>
2019-03-27 15:29:59 +0100

    updated tool that generates the list of the slides with a completed review workflow

--- promort/reviews_manager/management/commands/completed_reviews_slides_list.py
+++ promort/reviews_manager/management/commands/completed_reviews_slides_list.py
@@ -19,7 +19,7 @@
 
 from django.core.management.base import BaseCommand
 
-from reviews_manager.models import ReviewsComparison
+from reviews_manager.models import ClinicalAnnotation, ReviewsComparison
 
 import logging
 
@@ -32,10 +32,45 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('--output_file', dest='out_file', type=str, required=True,
                             help='the output file for the slides list')
+        parser.add_argument('--by_reviews_comparison', dest='by_reviews_comparison', action='store_true',
+                            help='use ReviewsComparison objects to decide if the review for a slide is completed, otherwise simply use ClinicalAnnotation')
         parser.add_argument('--linked_only', action='store_true',
                             help='write in the output file only slides linked to images on the OMERO server')
 
-    def _get_slide(self, reviews_comparison, ome_link_active):
+    def _get_slides_by_clinical_annotations(self, ome_link_active):
+        completed_slides_list = set()
+        clinical_annotations = ClinicalAnnotation.objects.all()
+        logger.info('Loaded %d ClinicalAnnotation objects', len(clinical_annotations))
+        for ca in clinical_annotations:
+            if ca.is_completed():
+                slides = self._get_clinical_annotation_slides(ca, ome_link_active)
+                completed_slides_list = completed_slides_list.union(slides)
+        logger.info('Found %s slides related to completed reviews', len(completed_slides_list))
+        return completed_slides_list
+
+    def _get_clinical_annotation_slides(self, clinical_annotation, ome_link_active):
+        slides = set()
+        for step in clinical_annotation.steps.all():
+            if ome_link_active:
+                if step.slide.omero_id is not None:
+                    slides.add(step.slide.id)
+            else:
+                slides.add(step.slide.id)
+        return slides
+
+    def _get_slides_by_review_comparisons(self, ome_link_active):
+        completed_slides_list = set()
+        review_comparisons = ReviewsComparison.objects.all()
+        logger.info('Loaded %d ReviewsComparison objects', len(review_comparisons))
+        for rc in review_comparisons:
+            if rc.is_completed() and not rc.is_evaluation_pending():
+                s = self._get_slide_by_reviews_comparison(rc, ome_link_active)
+                if s is not None:
+                    completed_slides_list.add(s)
+        logger.info('Found %s slides related to completed reviews', len(completed_slides_list))
+        return completed_slides_list
+
+    def _get_slide_by_reviews_comparison(self, reviews_comparison, ome_link_active):
         # check if all related ClinicalAnnotation objects were closed
         if not reviews_comparison.linked_reviews_completed():
             return None
@@ -50,20 +85,17 @@ class Command(BaseCommand):
         else:
             return slide.id
 
+    def _write_output_file(self, completed_slides_list, output_file):
+        logger.info('Writing output file')
+        with open(output_file, 'w') as ofile:
+            for slide in completed_slides_list:
+                ofile.write('%s\n' % slide)
+
     def handle(self, *args, **opts):
         logger.info('=== Exporting list of slides with a complete review workflow ===')
-        completed_slides_list = []
-        review_comparions = ReviewsComparison.objects.all()
-        logger.info('Loaded %d ReviewsComparison objects', len(review_comparions))
-        for rc in review_comparions:
-            if rc.is_completed() and not rc.is_evaluation_pending():
-                s = self._get_slide(rc, opts['linked_only'])
-                if s is not None:
-                    completed_slides_list.append(s)
-        logger.info('%d slides related to completed review workflows', len(completed_slides_list))
-        if len(completed_slides_list) > 0:
-            logger.info('Writing output file')
-            with open(opts['out_file'], 'w') as ofile:
-                for slide in completed_slides_list:
-                    ofile.write('%s\n' % slide)
-        logger.info('=== Export complete ===')
+        if opts['by_reviews_comparison']:
+            completed_slides_list = self._get_slides_by_review_comparisons(opts['linked_only'])
+        else:
+            completed_slides_list = self._get_slides_by_clinical_annotations(opts['linked_only'])
+        self._write_output_file(completed_slides_list, opts['out_file'])
+        logger.info('=== Job completed ===')
