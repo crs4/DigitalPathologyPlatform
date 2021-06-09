8faf04716bfcaeb3f3ec766c19b491fa0b7bdf1d
Luca Lianas <luca.lianas@crs4.it>
2021-06-09 10:50:30 +0200

    refactor: removed unused import

--- promort/view_templates/views.py
+++ promort/view_templates/views.py
@@ -17,11 +17,6 @@
 #  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 #  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-try:
-    import simplejson as json
-except ImportError:
-    import json
-
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
